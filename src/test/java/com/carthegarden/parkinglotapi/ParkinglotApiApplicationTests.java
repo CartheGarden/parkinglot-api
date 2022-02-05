db01dc98199521510ee530055bb7f8008395f02c
cranemont <cranemont@gmail.com>
2022-02-05 15:28:07 +0900

    Initialize

---
+++ src/test/java/com/carthegarden/parkinglotapi/ParkinglotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carthegarden.parkinglotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
