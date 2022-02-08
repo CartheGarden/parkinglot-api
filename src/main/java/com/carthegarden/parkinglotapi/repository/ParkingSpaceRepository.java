2dbac4a3b35a3f5bc3fa4ee07f2a1543b221ea63
cranemont <cranemont@gmail.com>
2022-02-08 18:50:31 +0900

    fix: Add repository annotation

--- src/main/java/com/carthegarden/parkinglotapi/repository/ParkingSpaceRepository.java
+++ src/main/java/com/carthegarden/parkinglotapi/repository/ParkingSpaceRepository.java
@@ -2,6 +2,8 @@ package com.carthegarden.parkinglotapi.repository;
 
 import com.carthegarden.parkinglotapi.domain.ParkingSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace, Long> {
 }
