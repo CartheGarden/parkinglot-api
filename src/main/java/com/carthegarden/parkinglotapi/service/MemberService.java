19b122ec40790e0a3d73ec82da2c35e344378ba6
minseo <alstj778@gmail.com>
2022-02-11 00:13:01 +0900

    feat: Implement memberLogin api

--- src/main/java/com/carthegarden/parkinglotapi/service/MemberService.java
+++ src/main/java/com/carthegarden/parkinglotapi/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.carthegarden.parkinglotapi.service;
 
+import com.carthegarden.parkinglotapi.domain.Member;
+import com.carthegarden.parkinglotapi.dto.MemberDto;
 import com.carthegarden.parkinglotapi.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,18 @@ public class MemberService {
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+
+    public Member loginMember(MemberDto.MemberLoginDto memberLoginDto) {
+
+        String naverId = memberLoginDto.getNaverId();
+        Member member = memberRepository.findByNaverId(naverId);
+        if(member == null) {
+            Member newMember = new Member.Builder(memberLoginDto).build();
+
+            member = memberRepository.save(newMember);
+        }
+
+        return member;
     }
 }
